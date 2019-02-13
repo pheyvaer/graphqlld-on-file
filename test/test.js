896cacb83575b197b64b66363aab6edc06001f4b
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-02-13 08:50:49 +0100

    increase timeout of tests

--- test/test.js
+++ test/test.js
@@ -7,6 +7,8 @@ const assert = require('assert');
 const lib = require('../index');
 
 describe('QueryExecutor', function() {
+  this.timeout(5000);
+
   it('basic', async () => {
     const result = await lib.query('test/test.ttl', {
       name: 'http://schema.org/name'
