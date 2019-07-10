51a50cf4de74f53bd0c37dedc6957932d0fab3f1
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-07-10 16:17:57 +0200

    update deps, add tests, add changelog

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@
 const assert = require('assert');
 const lib = require('../index');
 
-describe('QueryExecutor', function() {
+describe('QueryExecutor', function () {
   this.timeout(5000);
 
   it('basic', async () => {
@@ -15,5 +15,25 @@ describe('QueryExecutor', function() {
     }, '{ name }');
 
     assert.deepStrictEqual(result, [{name: ['Test']}]);
+  });
+
+  describe('optional', () => {
+    it('no results', async () => {
+      const result = await lib.query('test/test.ttl', {
+        name: 'http://schema.org/name',
+        author: 'http://schema.org/author'
+      }, '{ name author }');
+
+      assert.deepStrictEqual(result, []);
+    });
+
+    it('results', async () => {
+      const result = await lib.query('test/test.ttl', {
+        name: 'http://schema.org/name',
+        author: 'http://schema.org/author'
+      }, '{ name author @optional }');
+
+      assert.deepStrictEqual(result, [{name: ['Test']}]);
+    });
   });
 });
